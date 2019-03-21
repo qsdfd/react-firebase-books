d17154ef794b36994bfcfe0e65c1fc5f54d7019c
Kaisum <kaisumaro445@hotmail.com>
2019-03-21 12:55:15 +0100

    dependencies + firebase config added

---
+++ src/config/fb.config.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBOcCQOGsdcS48B1ZyUHAI-IZMo2qvfYnM",
+    authDomain: "books-166de.firebaseapp.com",
+    databaseURL: "https://books-166de.firebaseio.com",
+    projectId: "books-166de",
+    storageBucket: "books-166de.appspot.com",
+    messagingSenderId: "693151837283"
+};
+firebase.initializeApp(config);
